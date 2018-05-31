f6943b77827c20ddd4bb89300ef49f2237a4f094
Jamie Grafton <jamiegrafton@gmail.com>
2018-05-31 19:52:55 -0400

    Finalized grabbing info from hash created from xml api return

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,10 +1,11 @@
 class LocationsController < ApplicationController
 
     def show
-        @xml = HTTParty.get("https://api.greatschools.org/schools/nearby?key=d0bc7eeb56cba7074d643b67a977c968&state=ny&zip=10024&radius=5&limit=5")
+        xml = HTTParty.get("https://api.greatschools.org/schools/nearby?key=d0bc7eeb56cba7074d643b67a977c968&state=ny&zip=10024&radius=5&limit=5")
 
-        
-        # hash = Hash.from_xml()
+        hash = Hash.from_xml(xml.body)
+
+        @helper = hash["schools"]["school"]
 
 
 
