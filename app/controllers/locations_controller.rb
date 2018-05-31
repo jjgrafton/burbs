abbfe539eaf6d07c668646ee0a14528c47bc4ab9
Jamie Grafton <jamiegrafton@gmail.com>
2018-05-31 10:57:52 -0400

    Added scaffold for form for choosing location; added search API for Great Schools to locations controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,8 +1,8 @@
 class LocationsController < ApplicationController
     before_action :authenticate_user!
 
-    def new
-        
+    def show
+        @towns = HTTParty.get("https://api.greatschools.org/schools/nearby?key=d0bc7eeb56cba7074d643b67a977c968&zip=#{params[:zip]}&radius=5&limit=10")
     end
     
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
 class UsersController < ApplicationController
+
+    def index
+       #display user profile
+       #display form to choose location        
+    end
 end
