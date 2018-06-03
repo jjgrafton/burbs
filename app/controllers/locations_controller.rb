cda344b26b89ea9fb77b691917bcc388c067bb6d
Jamie Grafton <jamiegrafton@gmail.com>
2018-06-03 13:16:23 -0400

    Added extra school boxes, styled them, added links. School stuff done. Now to make Google Maps work…

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,4 +1,3 @@
-require "pp"
 class LocationsController < ApplicationController
 
     def show
@@ -16,46 +15,45 @@ class LocationsController < ApplicationController
 
         # @zip = params[:zip]
 
-        # if @zip = "10705"
-        #         @place_id = "place_id:ChIJlybmFmnywokRyL6hUK9g9OM"
-        # elsif @zip = "10701"
-        #         @place_id = "place_id:ChIJ_UMCtnDywokRH7B_1XIxOrw"
-        # elsif @zip = "10706"
-        #         @place_id = "place_id:ChIJq6qqqu7swokRHV5A9O9i9VY"
-        # elsif @zip = "10522"
-        #         @place_id = "place_id:ChIJDzJecoPswokR3K88ZtUA3BQ"
-        # elsif @zip = "10503"
-        #         @place_id = "place_id:ChIJo1uL-m_rwokRtEI1Kzk-Csw"
-        # elsif @zip = "10533"
-        #         @place_id = "place_id:ChIJxSxJ-jzrwokR1Tj-oGhA4h8"
-        # elsif @zip = "10591"
-        #         @place_id = "place_id:ChIJud0-Lr7qwokREHus8NEDiBI"
-        # elsif @zip = "10510"
-        #         @place_id = "place_id:ChIJ1e8DDovAwokRTM2dUmYTwBI"
-        # elsif @zip = "10562"
-        #         @place_id = "place_id:ChIJwbBXzsbAwokRKys2vmZUKoU"
-        # elsif @zip = "10520"
-        #         @place_id = "place_id:ChIJ5dwd2iPHwokRETICF3Y2p4U"
-        # elsif @zip = "10548"
-        #         @place_id = "place_id:ChIJLbM8VBDGwokRlM_iJiJbExM"
-        # elsif @zip = "10566"
-        #         @place_id = "place_id:ChIJlQF8JMzIwokREnhOAuXMQ7c"
-        # elsif @zip = "10524"
-        #         @place_id = "place_id:ChIJ7dxOwpnOwokR78PxkmzbDRI"
-        # elsif @zip = "10516"
-        #         @place_id = "place_id:ChIJ36U0gsrMwokRBDuP6vfNOzs"
-        # elsif @zip = "12508"
-        #         @place_id = "place_id: ChIJTc7w8p8z3YkRkEQmuhvlIdY"
-        # elsif @zip = "12590"
-        #         @place_id = "place_id:ChIJj10ugRk63YkRNQjLkA07eqE"
-        # elsif @zip = "12601"
-        #         @place_id = "place_id:ChIJPUcaod893YkRGvHemUKoEzE"
-        # end
+        if @zip = "10705"
+                @place_id = "place_id:ChIJlybmFmnywokRyL6hUK9g9OM"
+        elsif @zip = "10701"
+                @place_id = "place_id:ChIJ_UMCtnDywokRH7B_1XIxOrw"
+        elsif @zip = "10706"
+                @place_id = "place_id:ChIJq6qqqu7swokRHV5A9O9i9VY"
+        elsif @zip = "10522"
+                @place_id = "place_id:ChIJDzJecoPswokR3K88ZtUA3BQ"
+        elsif @zip = "10503"
+                @place_id = "place_id:ChIJo1uL-m_rwokRtEI1Kzk-Csw"
+        elsif @zip = "10533"
+                @place_id = "place_id:ChIJxSxJ-jzrwokR1Tj-oGhA4h8"
+        elsif @zip = "10591"
+                @place_id = "place_id:ChIJud0-Lr7qwokREHus8NEDiBI"
+        elsif @zip = "10510"
+                @place_id = "place_id:ChIJ1e8DDovAwokRTM2dUmYTwBI"
+        elsif @zip = "10562"
+                @place_id = "place_id:ChIJwbBXzsbAwokRKys2vmZUKoU"
+        elsif @zip = "10520"
+                @place_id = "place_id:ChIJ5dwd2iPHwokRETICF3Y2p4U"
+        elsif @zip = "10548"
+                @place_id = "place_id:ChIJLbM8VBDGwokRlM_iJiJbExM"
+        elsif @zip = "10566"
+                @place_id = "place_id:ChIJlQF8JMzIwokREnhOAuXMQ7c"
+        elsif @zip = "10524"
+                @place_id = "place_id:ChIJ7dxOwpnOwokR78PxkmzbDRI"
+        elsif @zip = "10516"
+                @place_id = "place_id:ChIJ36U0gsrMwokRBDuP6vfNOzs"
+        elsif @zip = "12508"
+                @place_id = "place_id: ChIJTc7w8p8z3YkRkEQmuhvlIdY"
+        elsif @zip = "12590"
+                @place_id = "place_id:ChIJj10ugRk63YkRNQjLkA07eqE"
+        elsif @zip = "12601"
+                @place_id = "place_id:ChIJPUcaod893YkRGvHemUKoEzE"
+        end
         
-        train = HTTParty.get("https://maps.googleapis.com/maps/api/directions/JSON?#{@place_id}&destination=place_id:ChIJhRwB-yFawokRi0AhGH87UTc&transit_mode=rail&key=AIzaSyD6LM1YiZNO8S1FixZXf9hKfAP2KS-V2w4")
-        pp train 
+        @train = HTTParty.get("https://maps.googleapis.com/maps/api/directions/JSON?origin=#{@place_id}&destination=place_id:ChIJhRwB-yFawokRi0AhGH87UTc&transit_mode=rail&key=AIzaSyD6LM1YiZNO8S1FixZXf9hKfAP2KS-V2w4")
 
-        driving = HTTParty.get("https://maps.googleapis.com/maps/api/directions/JSON?#{@place_id}&destination=place_id:ChIJhRwB-yFawokRi0AhGH87UTc&mode=driving&key=AIzaSyD6LM1YiZNO8S1FixZXf9hKfAP2KS-V2w4")
+        @driving = HTTParty.get("https://maps.googleapis.com/maps/api/directions/JSON?#{@place_id}&destination=place_id:ChIJhRwB-yFawokRi0AhGH87UTc&mode=driving&key=AIzaSyD6LM1YiZNO8S1FixZXf9hKfAP2KS-V2w4")
     end
     
 end
